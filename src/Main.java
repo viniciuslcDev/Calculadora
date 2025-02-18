ba2102bfe44c9dcbb01bd6f0a01f9abfd73b918a
vinicius.cabral <vinicius.c.lima2004@gmail.com>
2025-02-18 09:11:22 -0300

    Adiciona classe calculadora

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static int soma (int a, int b){
+        return a+b;
+    } public static void main(String[] args) {
+        System.out.println("Resultado" + soma(5,3));
+
+    }
+}
