0a17f59e0f1abb5db3d10c0280a97858beb6b56c
vinicius.cabral <vinicius.c.lima2004@gmail.com>
2025-02-18 09:40:02 -0300

    Adiciona método de subtração

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main{
     public static int soma (int a, int b){
         return a+b;
     } public static void main(String[] args) {
-        System.out.println("Resultado" + soma(5,3));
+        System.out.println("Resultado da soma " + soma(5,3));
+        System.out.println("Resultado da subtração " + subtracao(5,3));
 
+    } public static int subtracao (int a, int b){
+        return a - b;
     }
 }
